532aeaaec0a31ff16073b442420ac6889203d7dc
r-matsuzaka <matsuzakaryo@gmail.com>
2021-11-20 01:42:53 +0900

    feat:add train.py

--- src/cnn/train.py
+++ src/cnn/train.py
@@ -4,7 +4,8 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-from tensorflow.keras import datasets, layers, models
+from tensorflow.keras import layers, models
+from tensorflow.keras.utils import to_categorical
 
 home_path = Path(__file__).parents[2]
 train_data_path = home_path / "data/train"
@@ -76,3 +77,39 @@ test_images = load_image(test_size, test_file_paths)
 
 train_labels = load_label(str(train_label_path))
 test_labels = load_label(str(test_label_path))
+
+train_labels_onehot = to_categorical(train_labels, 10)
+test_labels_onehot = to_categorical(test_labels, 10)
+
+# ピクセルの値を 0~1 の間に正規化
+train_images, test_images = train_images / 255.0, test_images / 255.0
+num_classes = 10
+
+# LeNet
+model = models.Sequential()
+model.add(
+    layers.Conv2D(
+        filters=6,
+        kernel_size=(5, 5),
+        strides=(1, 1),
+        padding="same",
+        activation="tanh",
+        input_shape=(32, 32, 3),
+    )
+)
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(16, (5, 5), activation="relu"))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dense(120, activation="tanh"))
+model.add(layers.Dense(84, activation="tanh"))
+model.add(layers.Dense(num_classes, activation="softmax"))
+model.summary()
+
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(train_images, train_labels_onehot, epochs=200)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels_onehot, verbose=2)
+
+print(test_acc)
