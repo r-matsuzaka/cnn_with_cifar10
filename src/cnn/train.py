dcea7c376eba40771a2f4aab791547a49de0f17d
r-matsuzaka <matsuzakaryo@gmail.com>
2021-11-20 00:06:02 +0900

    feat:add load_label

--- src/cnn/train.py
+++ src/cnn/train.py
@@ -1,4 +1,78 @@
-import tensorflow as tf
+import glob
+from pathlib import Path
 
+import numpy as np
+import tensorflow as tf
+from PIL import Image
 from tensorflow.keras import datasets, layers, models
 
+home_path = Path(__file__).parents[2]
+train_data_path = home_path / "data/train"
+test_data_path = home_path / "data/test"
+data_path = home_path / "data"
+train_label_path = data_path / "train_label.txt"
+test_label_path = data_path / "test_label.txt"
+
+train_size = 8000
+test_size = 2000
+
+
+def load_image(num_image: int, image_paths: list) -> np.ndarray:
+    """
+    load image
+
+    Args:
+        num_image (int): number of images
+        image_paths (list): path list to a image
+
+
+    Returns:
+        images  (np.ndarray): numpy array of rgb value.
+        shape for train is (8000,32,32).
+        shape for test is (2000,32,32).
+    """
+
+    images = np.zeros((num_image, 32, 32, 3), "uint8")
+
+    for i, image in enumerate(image_paths):
+        img = Image.open(image)
+        img_array = np.asarray(img)
+
+        images[i, ...] = img_array
+
+    print(images.shape)
+
+    return images
+
+
+def load_label(label_path: str) -> np.ndarray:
+    """
+    load label
+
+    Args:
+        rgb_lst (list): list of rgb value
+
+
+    Returns:
+        labels_arr  (np.ndarray): numpy array of labels.
+        shape for train is (8000,).
+        shape for test is (2000,).
+    """
+
+    with open(label_path) as f:
+        labels = f.read()
+        label_lst = list(labels)
+        label_lst = [int(i) for i in label_lst]
+        labels_arr = np.array(label_lst)
+
+    return labels_arr
+
+
+train_file_paths = glob.glob(str(train_data_path / "*"))
+test_file_paths = glob.glob(str(test_data_path / "*"))
+
+train_images = load_image(train_size, train_file_paths)
+test_images = load_image(test_size, test_file_paths)
+
+train_labels = load_label(str(train_label_path))
+test_labels = load_label(str(test_label_path))
--- src/utils/unpack.py
+++ src/utils/unpack.py
@@ -1,12 +1,13 @@
+import os
 import pickle
 from pathlib import Path
 
 import numpy as np
 from PIL import Image
-import os
 
 home_path = Path(__file__).parents[2]
-data_path = home_path / "cifar-10-batches-py/data_batch_1"
+source_path = home_path / "cifar-10-batches-py/data_batch_1"
+data_path = home_path / "data"
 train_data_path = home_path / "data/train"
 test_data_path = home_path / "data/test"
 img_size = 1024
@@ -22,7 +23,6 @@ def create_dir() -> None:
     os.makedirs(test_data_path, exist_ok=True)
 
 
-
 def create_rgb_array(rgb_lst: list) -> np.ndarray:
     """
     Create numpy array of rgb value
@@ -32,14 +32,12 @@ def create_rgb_array(rgb_lst: list) -> np.ndarray:
 
 
     Returns:
-        rgb_arr (np.ndarray): numpy array of rgb value. size is (32,32,3).
-    -----------------
-
+        rgb_arr (np.ndarray): numpy array of rgb value. shape is (32,32,3).
 
     """
 
     create_dir()
-    
+
     r_lst = rgb_lst[:img_size]
     r_arr = np.array(r_lst)
     r_arr = r_arr.reshape([32, 32])
@@ -60,21 +58,29 @@ def create_rgb_array(rgb_lst: list) -> np.ndarray:
     return rgb_arr
 
 
-with open(data_path, "rb") as fo:
-    dict = pickle.load(fo, encoding="bytes")
+train_label_path = data_path / "train_label.txt"
+test_label_path = data_path / "test_label.txt"
+
+with open(source_path, "rb") as fd, open(train_label_path, mode="w") as ftr, open(
+    test_label_path, mode="w"
+) as fte:
+    dict = pickle.load(fd, encoding="bytes")
+
+    rgb_lst = dict[b"data"]
+    label_lst = dict[b"labels"]
 
     for i in range(train_size):
-        rgb_lst = dict[b"data"][i]
-        rgb_arr = create_rgb_array(rgb_lst)
+        rgb_arr = create_rgb_array(rgb_lst[i])
         pil_image_color = Image.fromarray(rgb_arr)
 
         pil_image_color.save(train_data_path / f"train_{i}.png")
+        ftr.write(f"{label_lst[i]}")
 
     j = 0
     for i in range(train_size, 10000):
-        rgb_lst = dict[b"data"][i]
-        rgb_arr = create_rgb_array(rgb_lst)
+        rgb_arr = create_rgb_array(rgb_lst[i])
         pil_image_color = Image.fromarray(rgb_arr)
 
         pil_image_color.save(test_data_path / f"test_{j}.png")
+        fte.write(f"{label_lst[i]}")
         j += 1
